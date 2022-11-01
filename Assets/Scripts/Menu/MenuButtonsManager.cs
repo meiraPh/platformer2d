e50fbd5c60dddf28d7344a8d5f3a0154096b0c89
Pedro H Meira <pedrohmeirasilva@gmail.com>
2022-11-01 14:51:14 -0300

    add textmesh pro to the project

---
+++ Assets/Scripts/Menu/MenuButtonsManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class MenuButtonsManager : MonoBehaviour
+{
+    public List<GameObject> buttons;
+
+
+    [Header("Animation")]
+    public float duration = .2f;
+    public float delay = .05f;
+    public Ease ease = Ease.OutBack;
+
+    private void Awake(){
+        HideButtons();
+        ShowButtons();
+    }
+
+
+    private void HideButtons()
+    {
+        foreach(var b in buttons)
+        {
+            b.transform.localScale = Vector3.zero;
+            b.SetActive(false);
+        }
+    }
+    private void ShowButtons()
+    {
+
+        for (int i=0; i < buttons.Count; i++)
+        {
+            var b = buttons[i];
+            b.SetActive(true);
+            b.transform.DOScale(1, duration).SetDelay(i*delay).SetEase(ease);
+        }
+    }
+
+}
