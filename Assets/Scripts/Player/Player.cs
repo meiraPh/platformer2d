c9b5206ee8ee1091223ab3a5312e0570355c4c6c
Pedro H Meira <pedrohmeirasilva@gmail.com>
2022-11-03 10:42:46 -0300

    add jump to prototype

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
-    //public Vector2 velocity;
+    public Vector2 friction = new Vector2 (.1f, 0);
 
     public float speed;
+
+    public float forceJump = 2;
     
     private void Update()
+    {
+        HandleJump();
+        HandleMovement();
+    }
+
+    private void HandleMovement()
     {
         if(Input.GetKey(KeyCode.LeftArrow))
         {
@@ -18,6 +26,23 @@ public class Player : MonoBehaviour
         else if(Input.GetKey(KeyCode.RightArrow))
         {
             myRigidbody.velocity = new Vector2(speed, myRigidbody.velocity.y);
+        }
+
+        if(myRigidbody.velocity.x > 0)
+        {
+            myRigidbody.velocity += friction;
+        }
+        else if(myRigidbody.velocity.x < 0)
+        {
+            myRigidbody.velocity -= friction;
+        }
+    }
+
+    private void HandleJump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.velocity = Vector2.up* forceJump;
         }
     }
 }
