12278c1cfc1a8af2e8c9892e1c3cc4a7ee03d17b
Pedro H Meira <pedrohmeirasilva@gmail.com>
2022-12-07 20:45:13 -0300

    Update ItemManager

--- Assets/Scripts/Items/ItemManager.cs
+++ Assets/Scripts/Items/ItemManager.cs
@@ -1,21 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Company.Core.Singleton;
 
-public class ItemManager : MonoBehaviour
+public class ItemManager : Singleton<ItemManager>
 {
-    public static ItemManager Instance;
-        
     public int coins;
-
-    private void Awake()
-    {
-        if(Instance == null)
-            Instance = this;
-        else   
-            Destroy(gameObject);
-
-    }
 
     private void Start()
     {
