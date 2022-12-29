93f75384560c498959f14dbf763ce2a3d9d36841
Pedro H Meira <pedrohmeirasilva@gmail.com>
2022-12-29 11:46:57 -0300

    Update UI/coins in Interface
    Using classic mode for implementation

--- Assets/Scripts/Items/ItemManager.cs
+++ Assets/Scripts/Items/ItemManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using Company.Core.Singleton;
 
 public class ItemManager : Singleton<ItemManager>
 {
     public int coins;
+    public TextMeshProUGUI uiTextCoins;
 
     private void Start()
     {
@@ -20,6 +22,12 @@ public class ItemManager : Singleton<ItemManager>
     public void AddCoins(int amount = 1)
     {
         coins += amount;
+        UpdateUi();
+    }
+
+    private void UpdateUi()
+    {
+        uiTextCoins.text = coins.ToString();
     }
 
 }
